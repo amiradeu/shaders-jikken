235e3cefaa9c8ac60789ca35602c7036e4f4fe2b
Amira Deuraseh <amiradeu.x@gmail.com>
2025-01-29 21:34:43 +0800

    🎨 Line Inversion > draw black lines on white
    a trick to handle when lineWidth > 0.5

--- grid-floor/src/shaders/base/fragment.glsl
+++ grid-floor/src/shaders/base/fragment.glsl
@@ -17,12 +17,18 @@ void main()
     //float lineAA = fwidth(uv.x);
     vec2 uvDeriv = fwidth(uv);
 
+    // 💡 Invert Line Trick
+    // since 0.5 clamp was use, to handle line thickness > 0.5
+    // draw black lines on white offset by half a grid width
+    bool invertLine = lineWidth.x > 0.5;
+    vec2 targetWidth = invertLine ? 1.0 - lineWidth : lineWidth;
+
     // 💡 Phone-wire AA
     // STEP 1: ensure line does not get smaller than one pixel
     // if so, we will clamp it to one pixel
     // vec2 drawWidth = max(uvDeriv, lineWidth);
     // clamp to 0.5 to ensure line fades to grey, not black
-    vec2 drawWidth = clamp(lineWidth, uvDeriv, vec2(0.5));
+    vec2 drawWidth = clamp(targetWidth, uvDeriv, vec2(0.5));
 
     // 💡 1 pixel wide smoothstep can be too sharp causing aliasing
     // hence using 1.5 pixel wide smoothstep
@@ -34,7 +40,8 @@ void main()
     // 👉 0-1-0(shift) make white at center(0,0) position
     // (fract) - make sawtooth wave
     //float lineUV = 1.0 - abs(fract(uv.x) * 2.0 - 1.0);
-    vec2 gridUV = 1.0 - abs(fract(uv) * 2.0 - 1.0);
+    vec2 gridUV = abs(fract(uv) * 2.0 - 1.0);
+    gridUV = invertLine ? gridUV : 1.0 - gridUV;
 
     //💡 repeating lines
     // use the derivative to make the lines smooth
@@ -44,15 +51,16 @@ void main()
     // 💡 Phone-wire AA
     // STEP 2: fades the line out as it gets thinner
     // how thick we want divided by how thick we’re drawing
-    grid2 *= clamp(lineWidth / drawWidth, 0.0, 1.0);
+    grid2 *= clamp(targetWidth / drawWidth, 0.0, 1.0);
 
     // 💡 Moire Suppresion
     // grid cells < a pixel(when derivative > 1.0), moire pattern can appear
     // note: after the 0.5 clamp, moire would be more pronounced, but in my case, i do not see any moire
     // fade to solid color when 0.5 > derivative > 1.0 
     // anti-aliased lines start to merge
-    grid2 = mix(grid2, lineWidth, clamp(uvDeriv * 2.0 - 1.0, 0.0, 1.0));
-
+    grid2 = mix(grid2, targetWidth, clamp(uvDeriv * 2.0 - 1.0, 0.0, 1.0));
+    grid2 = invertLine ? 1.0 - grid2 : grid2;
+    
     // overlap xy lines
     float grid = mix(grid2.x, 1.0, grid2.y);
     
