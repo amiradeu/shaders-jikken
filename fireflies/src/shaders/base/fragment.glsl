2d9e92283ec80ae0939d4eced265a500763ef6b5
Amira Deuraseh <amiradeu.x@gmail.com>
2025-05-21 00:18:38 +0800

    🎨 Flicker > interesting maths patterns

--- fireflies/src/shaders/base/fragment.glsl
+++ fireflies/src/shaders/base/fragment.glsl
@@ -5,8 +5,12 @@ uniform float uFlickerSync;
 
 varying float vRandomness;
 
-// Generic Noise
-float rand(float n){return fract(sin(n) * 43758.5453123);}
+#define PI 3.1415926535897932384626433832795
+
+float lerp(float begin, float end, float value) {
+    return (1.0 - value) * begin + value * end;
+}
+
 
 void main()
 {
@@ -14,11 +18,21 @@ void main()
     float strength = distance(gl_PointCoord, vec2(0.5));
     strength = 1.0 - strength;
     strength = pow(strength, 10.0);
+    // prevent boxy circle blur
+    strength = smoothstep(0.0, 0.99, strength);
 
     // 💡 Random phase per firefly
     // phase shift - higher value, less synchronised blinking pattern
     // sin oscillates from [0,1]
     float flicker = sin(uTime * uFlickerSpeed + vRandomness * uFlickerSync) * 0.5 + 0.5;
+    
+    // interesting curve for blinking pattern
+    // flicker = 1.0 - pow(abs(sin(PI * (uTime * uFlicker + vRandomness * uFlickerSync) / 2.0) * 0.5 + 0.5), 0.5);
+    flicker = pow(cos(PI * (uTime * (uFlickerSync * 0.1 * vRandomness)+ vRandomness * uFlickerSync) / 2.0) * 0.5 + 0.5, 0.5);
+    // flicker = 1.0 - pow(abs(sin(PI * (uTime * uFlicker + vRandomness * uPhasuFlickerSynceShift) / 2.0) * 2.0 - 1.0), 0.5);
+
+    // brightness range
+    flicker = lerp(10.0, 0.1, flicker);
 
     // 💡 Colored point
     vec3 color = mix(vec3(0.0), uColor, strength) * flicker;
